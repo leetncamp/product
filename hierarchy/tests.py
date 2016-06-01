5096fa9d3de05f1e09b215562f36d97b62a82bf1
Lee Campbell <lee@salk.edu>
2016-06-01 08:11:31 -0700

    adding tests

--- hierarchy/tests.py
+++ hierarchy/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from hierarchy.models import *
+import os
+import requests
 
 # Create your tests here.
+
+class ImportExportTestCase(TestCase)
+    def setUp(self):
+        os.system(""./resetDB")
