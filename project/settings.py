e7daea8cd6b9289a03de29970e4e722ba7a30548
Lee Campbell <lee@salk.edu>
2016-06-25 11:32:31 -0700

    got rid of templates setting warning

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,28 @@ SECRET_KEY = 'f1wg3=b#k^s1v4)t427n*r^%55e#q$gt-4+pete+7)^x%vp&k2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 ALLOWED_HOSTS = []
 
