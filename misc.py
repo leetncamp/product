e9668d15d8eccb6c108a99e303c8960cbfef1844
Lee Campbell <lee@salk.edu>
2016-06-27 11:31:33 -0700

    remove chdir from misc.py

--- misc.py
+++ misc.py
@@ -12,7 +12,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 application = get_wsgi_application()
 from django import setup
-os.chdir(os.path.dirname(__file__))
+#os.chdir(os.path.dirname(__file__))
 setup()
 from hierarchy.models import *
 
